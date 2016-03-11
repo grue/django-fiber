6d6b707a7ecbc9af01969e928e125f02fa2c533a
Joshua Sorenson <code.poet@gmail.com>
2016-03-10 21:02:55 -0800

    Handle 1.9 naming change in django.contrib.admin.util -> django.contrib.admin.utils Tests at least run for 1.9 now.

--- fiber/admin.py
+++ fiber/admin.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.contrib import admin, messages
-from django.contrib.admin.util import model_ngettext
+try:
+    from django.contrib.admin.util import model_ngettext
+except ImportError:
+    from django.contrib.admin.utils import model_ngettext
+
 from django.utils.translation import ugettext_lazy as _
 from django.db.models.deletion import ProtectedError
 from django.core.exceptions import PermissionDenied
